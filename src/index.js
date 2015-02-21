670dda4b7be4686e9fc17af93dade02a376068a3
Nathan Faucett <nathanfaucett@gmail.com>
2015-02-21 10:16:58 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-var indexOf = require("index_of"),
-    extend = require("extend"),
-    isFunction = require("is_function"),
+var extend = require("extend"),
     emptyFunction = require("empty_function"),
     environment = require("environment");
 
@@ -28,7 +26,9 @@ var window = environment.window,
                 throw new Error("XMLHttpRequest not supported by this browser");
             }
 
-            return createXMLHttpRequestFactory(type);
+            return function XMLHttpRequest() {
+                return type();
+            };
         }([
             function createActiveObject() {
                 return new ActiveXObject("Msxml2.XMLHTTP");
@@ -42,79 +42,8 @@ var window = environment.window,
         ]))
     );
 
-function createXMLHttpRequestFactory(createXMLHttpRequest) {
 
-    function XMLHttpRequest() {
-        var xhr = createXMLHttpRequest();
-
-        function onreadystatechange(e) {
-            var statusCode = +xhr.status,
-                readyState = +xhr.readyState;
-
-            xhr.dispatchEvent("readystatechange", e);
-            xhr.dispatchEvent("progress", e);
-
-            if (readyState === 4) {
-                if ((statusCode > 199 && statusCode < 301) || statusCode === 304) {
-                    xhr.dispatchEvent("load", e);
-                } else {
-                    xhr.dispatchEvent("error", e);
-                }
-            }
-        }
-
-        if (isFunction(xhr.attachEvent)) {
-            xhr.attachEvent("onreadystatechange", onreadystatechange, true);
-        } else {
-            xhr.onreadystatechange = onreadystatechange;
-        }
-
-        xhr.setRequestHeader = emptyFunction;
-        xhr.addEventListener = addEventListener;
-        xhr.removeEventListener = removeEventListener;
-        xhr.dispatchEvent = dispatchEvent;
-
-        return xhr;
-    }
-
-    XMLHttpRequest.prototype = XMLHttpRequestPolyfill.prototype;
-    XMLHttpRequest.prototype.constructor = XMLHttpRequest;
-
-    return XMLHttpRequest;
-}
-
-function addEventListener(name, fn) {
-    var events = this.__events__ || (this.__events__ = {}),
-        eventList = events[name] || (events[name] = []);
-
-    if (isFunction(fn)) {
-        eventList[eventList.length] = fn;
-    }
-}
-
-function removeEventListener(name, fn) {
-    var events = this.__events__ || (this.__events__ = {}),
-        eventList = events[name] || (events[name] = []),
-        index = indexOf(eventList, fn);
-
-    if (index !== -1) {
-        eventList.splice(index, 1);
-    }
-}
-
-function dispatchEvent(name, e) {
-    var events = this.__events__ || (this.__events__ = {}),
-        eventList = events[name] || (events[name] = []),
-        i = -1,
-        length = eventList.length - 1,
-        event;
-
-    while (i++ < length) {
-        event.call(this, e);
-    }
-}
-
-XMLHttpRequestPolyfill.prototype.nativeSetRequestHeader = XMLHttpRequestPolyfill.prototype.setRequestHeader;
+XMLHttpRequestPolyfill.prototype.nativeSetRequestHeader = XMLHttpRequestPolyfill.prototype.setRequestHeader || emptyFunction;
 
 XMLHttpRequestPolyfill.prototype.setRequestHeader = function setRequestHeader(key, value) {
     (this.__requestHeaders__ || (this.__requestHeaders__ = {}))[key] = value;
